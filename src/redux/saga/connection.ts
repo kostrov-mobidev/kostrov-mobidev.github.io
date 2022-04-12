952d7ed7d2a45d18e74f94eb69415b86847f2e06
Oleksandr Kostrov <o.kostrov@mobidev.biz>
2022-04-12 11:18:54 +0200

    fix: sokcet auto reconnect

--- src/redux/saga/connection.ts
+++ src/redux/saga/connection.ts
@@ -6,26 +6,12 @@ import { handleGameAction } from "../actions/basics/basics";
 
 const createConnection = (socket: any): any =>
   eventChannel((emit) => {
-    socket.onopen = function (e: any) {
-      console.log("[open] Соединение установлено");
-      console.log("Отправляем данные на сервер");
-      socket.send("help");
-    };
+    socket.onopen = function (e: any) {};
 
     const onMessage = (event: any) => {
       emit(event.data);
     };
     socket.onmessage = onMessage;
-    socket.onclose = function (event: any) {
-      if (event.wasClean) {
-        console.log(
-          `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`
-        );
-      } else {
-        console.log("[close] Соединение прервано");
-      }
-    };
-
     socket.onerror = function (error: any) {
       console.log(`[error] ${error}`);
     };
@@ -39,15 +25,10 @@ function* watchOnActions(): any {
 
   while (true) {
     try {
-      // An error from socketChannel will cause the saga jump to the catch block
       const payload: any = yield take(socketChannel);
       yield put(handleGameAction(payload));
-      //   yield fork(pong, socket);
     } catch (err) {
       console.error("socket error:", err);
-      // socketChannel is still open in catch block
-      // if we want end the socketChannel, we need close it explicitly
-      // socketChannel.close()
     }
   }
 }
